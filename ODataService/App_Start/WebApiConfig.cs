ece33fccd51e7bc10e3724da0145c979e77ae6af
Markus Kollers <mail@markuskollers.de>
2017-08-15 16:23:11 +0200

    Migrate to VS2017 + lower camel case serializer

--- ODataService/App_Start/WebApiConfig.cs
+++ ODataService/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -15,7 +16,8 @@ namespace ODataService
 			// Web API routes
 			config.MapHttpAttributeRoutes();
 
-			config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 		}
 	}
 }
